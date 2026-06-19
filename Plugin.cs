e4d01d8d0d8a69cb96e7c67abe5a50e6c5e054ec
NaeNae <naenaetartbusiness@gmail.com>
2026-06-19 10:04:19 +0300

    feat: Add six immersive pre-registered dialogue modes

--- Plugin.cs
+++ Plugin.cs
@@ -32,7 +32,56 @@ namespace DialLib
                 "This is my last stand... and I'm taking you down with me!"
             });
 
-            Log.LogInfo("Default 'laststand' dialogue registered successfully with DialLib.");
+            // 1. Boss / Heavy Threat Spotted Mode
+            DialogueRegistry.RegisterDialogue("boss_spotted", new List<string>
+            {
+                "What... what is that thing?!",
+                "That is NOT a normal specimen! Look at the size of it!",
+                "I don't think my <weapon> is going to be enough for this...",
+                "My heart is at <adrenaline> adrenaline just looking at that monster!"
+            });
+
+            // 2. Out of Ammo / Reloading Mode
+            DialogueRegistry.RegisterDialogue("low_ammo", new List<string>
+            {
+                "Click... empty! Need to reload!",
+                "I'm out! Covering fire while I load my <weapon>!",
+                "Need a fresh mag! Just give me a second!"
+            });
+
+            // 3. Heavy Bleeding Mode
+            DialogueRegistry.RegisterDialogue("critical_bleed", new List<string>
+            {
+                "I'm leaking... so much blood...!",
+                "I need a bandage now! I've lost <blood> of my blood!",
+                "Everything is turning red... I'm bleeding out!"
+            });
+
+            // 4. Fracture / Bone Break Mode
+            DialogueRegistry.RegisterDialogue("broken_bone", new List<string>
+            {
+                "Crack... my <limb>! I think it's broken!",
+                "Ah! I can't move my <limb>!",
+                "That hurt! My <limb> is fractured!"
+            });
+
+            // 5. Treatment / Healing Mode
+            DialogueRegistry.RegisterDialogue("healed_limb", new List<string>
+            {
+                "Bandage is secure. My <limb> feels a bit better.",
+                "Applied the dressing. Pain is down to <pain>.",
+                "That's patched up. Back to the fight!"
+            });
+
+            // 6. Stumbling / Tripping Mode
+            DialogueRegistry.RegisterDialogue("stumble", new List<string>
+            {
+                "Whoa! Watch your footing!",
+                "Tripped on the loose rocks! Nearly broke my <limb>!",
+                "Ugh! Stupid loose gravel!"
+            });
+
+            Log.LogInfo("Default dialogue modes ('laststand', 'boss_spotted', 'low_ammo', 'critical_bleed', 'broken_bone', 'healed_limb', 'stumble') registered successfully with DialLib.");
         }
 
         private void OnDestroy()
